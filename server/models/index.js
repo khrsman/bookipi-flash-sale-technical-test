87ba02b35a04863ec501686a703bbd4a65f52f51
khrsman <khrsman@gmail.com>
2026-02-11 18:42:13 +0700

    add purchase model

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,13 @@
+/**
+ * Models Index
+ * Centralized export for all Mongoose models
+ * Allows clean importing: const { FlashSale, Purchase } = require('./models')
+ */
 
 const FlashSale = require('./FlashSale');
+const Purchase = require('./Purchase');
 
 module.exports = {
   FlashSale,
+  Purchase
 };
