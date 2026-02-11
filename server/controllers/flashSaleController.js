6c802f4fbc15311b9e50c71ad749102e2acff771
khrsman <khrsman@gmail.com>
2026-02-12 01:34:55 +0700

    add check purchased item controller

--- server/controllers/flashSaleController.js
+++ server/controllers/flashSaleController.js
@@ -37,4 +37,15 @@ exports.attemptPurchase = async (req, res) => {
   }
 };
 
-
+/**
+ * Check if user purchased (single product)
+ */
+exports.checkPurchase = async (req, res) => {
+  try {
+    const userIdentifier = req.params.userIdentifier;
+    const result = await flashSaleService.checkUserPurchase(userIdentifier);
+    res.json({ success: true, data: result });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
